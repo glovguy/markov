47997328bf9e9b8ac49f001a9a73db267aea8de2
Karl <karlsmith@bouzou.com>
2018-04-09 23:33:35 -0400

    Adds plagiarism trie pickles

--- pickler.py
+++ pickler.py
@@ -1,6 +1,7 @@
 import pickle
 import spacy
 from src.markov import stack_markov, regular_markov
+from src.plagiarism.plagiarism import build_plagiarism_trie
 
 
 nlp = spacy.load('en_core_web_sm')
@@ -14,11 +15,12 @@ def read_file(filename):
 
 eitherOr = read_file('data/corpora/EitherOr.txt')
 
-apology = read_file('data/corpora/apology.txt')
-republic = read_file('data/corpora/republic.txt')
 symposium = read_file('data/corpora/symposium.txt')
+republic = read_file('data/corpora/republic.txt')
+apology = read_file('data/corpora/apology.txt')
 
 
+# Regular Markov Chain
 regular_chain = regular_markov.build_chain(symposium)
 regular_chain = regular_markov.build_chain(republic, regular_chain)
 regular_chain = regular_markov.build_chain(apology, regular_chain)
@@ -27,9 +29,24 @@ with open('data/pickles/plato_regular_chain.pkl', 'wb') as myf:
     pickle.dump(regular_chain, myf)
 
 
+# Stack Markov Chain
 stack_chain = stack_markov.build_chain(symposium)
 stack_chain = stack_markov.build_chain(republic, stack_chain)
 stack_chain = stack_markov.build_chain(apology, stack_chain)
 
 with open('data/pickles/plato_stack_chain.pkl', 'wb') as myf:
     pickle.dump(stack_chain, myf)
+
+
+# Plagiarism Trie
+plato_trie = build_plagiarism_trie(symposium)
+plato_trie = build_plagiarism_trie(republic, plato_trie)
+plato_trie = build_plagiarism_trie(apology, plato_trie)
+
+with open('data/pickles/plato_plagiarism_trie.pkl', 'wb') as myf:
+    pickle.dump(plato_trie, myf)
+
+kierkegaard_trie = build_plagiarism_trie(eitherOr)
+
+with open('data/pickles/kierkegaard_plagiarism_trie.pkl', 'wb') as myf:
+    pickle.dump(kierkegaard_trie, myf)
